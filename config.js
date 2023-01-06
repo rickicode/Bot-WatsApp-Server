2727dd68e967eefd02842c6c24fff543353b678b
Ricki <rickicode@gmail.com>
2023-01-06 22:31:04 +0700

    first commit

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    API_KEY_RM_BG: "EZ3HDswwbfy7tXgKTfmhbAdY",
-    API_KEY_OPEN_AI: "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"
+    API_KEY_RM_BG: "YOUR_API_KEY",
+    API_KEY_OPEN_AI: "YOUR_API_KEY"
 }
